6c9b4445be36b24d8fb5dfd3febac4bc7942b590
shpak <serg.shp@gmail.com>
2019-03-13 16:19:16 +0200

    Example03

---
+++ src/day001/example03Exception.py
@@ -0,0 +1,19 @@
+try:
+    print(7 / 0)
+except ZeroDivisionError:
+    print('Поймано исключение - деление на ноль')
+except:
+    print('Поймано какое-то исключение')
+
+finally:
+    print('Блок Finally')
+
+try:
+    if True:
+        raise TypeError
+except TypeError:
+    print('Поймано наше исключение')
+
+    print('Программа завершена')
+
+
