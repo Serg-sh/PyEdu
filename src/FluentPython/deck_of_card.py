4916c2325807149df1a2b88865a8a82f934f4946
Shpak <serg.shp@gmail.com>
2020-03-21 01:45:50 +0200

    print sep=''

--- src/FluentPython/deck_of_card.py
+++ src/FluentPython/deck_of_card.py
@@ -3,10 +3,10 @@ from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
+
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
-
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
@@ -17,17 +17,18 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
-suit_values = dict(spades=3,hearts=2,diamonds=1, clubs=0)
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
 def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
 
 
-
 beer_card = Card('7', 'diamonds')
 
 print(beer_card)
-
 
 deck = FrenchDeck()
 
@@ -47,6 +48,11 @@ for card in sorted(deck, key=spades_high):
 print()
 print()
 
+
 for card in reversed(deck):
     print(card)
 
+print()
+print()
+
+print(1, 2, 3, 4, 5, sep='--')
