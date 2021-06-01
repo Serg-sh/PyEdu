3b8e6e9863cdad61b1c37abce6cbaed2f09ea680
shpak <serg.shp@gmail.com>
2021-06-01 17:14:46 +0300

    inLineKeybord

--- src/teleBot/aiogram/001_first_bot/main.py
+++ src/teleBot/aiogram/001_first_bot/main.py
@@ -11,7 +11,8 @@ dp = Dispatcher(bot, loop=loop)
 
 if __name__ == '__main__':
     from handlers import dp, send_to_admin
-    executor.start_polling(dp,on_startup=send_to_admin)
+    executor.start_polling(dp, on_startup=send_to_admin)
+
 
 
 
