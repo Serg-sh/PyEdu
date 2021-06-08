cd3cc2dfeb77505b1c3876cacaae96283a903247
shpak <serg.shp@gmail.com>
2021-06-08 12:48:17 +0300

    FSM_2

--- src/teleBot/aiogram/_005_StateMashine/app.py
+++ src/teleBot/aiogram/_005_StateMashine/app.py
@@ -1,18 +1,5 @@
-from aiogram import executor
-
-from loader import dp
-import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
-
-
-async def on_startup(dispatcher):
-    # Устанавливаем дефолтные команды
-    await set_default_commands(dispatcher)
-
-    # Уведомляет про запуск
-    await on_startup_notify(dispatcher)
-
-
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    from aiogram import executor
+    from src.teleBot.aiogram._005_StateMashine.handlers.users.testing import dp, send_to_admin
+
+    executor.start_polling(dp, on_startup=send_to_admin)
